34eb338fa44e234b61c5828d04f4790888d6cee9
sidharth07-tech <sidharth221107@gmail.com>
2026-05-12 20:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -313,7 +313,10 @@ def view_note(note_id):
     return render_template("note_detail.html", note=note, user=user)
 
 
+with app.app_context():
+    init_db()
+
+
 if __name__ == "__main__":
-    with app.app_context():
-        init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
